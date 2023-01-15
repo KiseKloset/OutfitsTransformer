44dec63a1549580ca29e970ce3913253d840417e
TÙNG PHAN NGUYỄN THANH <19120424@student.hcmus.edu.vn>
2023-01-15 22:20:42 +0700

    Finish loss

--- model/loss.py
+++ model/loss.py
@@ -1,5 +1,11 @@
-import torch.nn.functional as F
+import torch.nn as nn
 
-
-def nll_loss(output, target):
-    return F.nll_loss(output, target)
+# output: [batch_size, num_categories, d_model]
+# embeddings: [batch_size, num_categories, d_model]
+# target_mask: [batch_size, num_categories]
+def mse(output, embeddings, target_mask):
+    mask = target_mask[:, :, None].float()
+    y_hat = output * mask
+    y = embeddings * mask
+    return nn.MSELoss(y_hat, y)
+    
